a80a25421d49749e65e84f865b38349ba7c9cd8c
EOFL Happened <genBTC@gmx.com>
2020-05-04 14:05:49 -0400

    Improve script for more edge cases

--- README.txt
+++ README.txt
@@ -4,9 +4,16 @@ git add sizesort.py && git commit
 
 ./sizesort.py > Hmovies.csv
 
+#pre-requisities for binpacking
 sudo apt-get install python3-pip
 pip3 install setuptools wheel
-# https://pypi.org/project/binpacking/
+# need https://pypi.org/project/binpacking/ (also installs numpy, future,)
 pip3 install binpacking
-/home/genr8eofl/.local/bin/binpacking -h 
-/home/genr8eofl/.local/bin/binpacking -f Hmovies.csv -V 24159191040 -c 0
+~/.local/bin/binpacking -h 
+~/.local/bin/binpacking -f Hmovies.csv -V 24159191040 -c 0
+
+./sizesort.py > 4TB.csv 
+# + add weight,name to main file -H header, and act as a seperator. Lower bound 700MB, upper bound = maxbinsize
+~/.local/bin/binpacking -f 4TB.csv -V 24159191040 -c 0 -l 700000000 -u 24159191040 -H > 4TB-groups.txt
+cat 4TB_* >> 4TB-grouped.csv
+
--- sizesort.py
+++ sizesort.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 import os
 
-def getSize(filename):
-    st = os.stat(filename)
-    return st.st_size
-
 # pick a folder
 #TODO: take pathname on command line
 #folder = os.getcwd()
 homedir = os.path.expanduser('~')
 #folder = os.path.join(homedir,'4TB/Movies/unseeded')
-folder = os.path.join(homedir,'4TB/Movies/H-WD500-MOVIES')
+#folder = os.path.join(homedir,'4TB/Movies/H-WD500-MOVIES')
+folder = os.path.join(homedir,'4TB/Movies/')
+
+def getSize(filename):
+    return os.stat(filename).st_size
 
 def getOneLevelSizes(folder):
   folder_list = os.listdir(path=folder)
   paths = [os.path.join(folder, name) for name in folder_list]
-  #sizes = [(path, os.stat(path).st_size) for path in paths]
   sizes = []
   for path in paths:
     if os.path.isfile(path):
-      if (('.mkv' in path) or ('.mp4' in path)) and not ('sample' in path.lower()):
+      if ('unseeded' not in path.lower()):
+        continue
+      blacklist_words = ['sample','.m2ts','.sub','.nfo','.dat','.srt']
+      if any(skip in path.lower() for skip in blacklist_words):
+        continue
+      filetypes = ['.mkv','.mp4','.avi']
+      if (t for t in filetypes if t in path.lower()):
         sizes.append((getSize(path), path))
     else: #recurse
       sizes += getOneLevelSizes(path)
